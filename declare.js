960f0ba1a1588d0f3c8b360844b938e4e58aec6d
Bald Mike <baldmike312@gmail.com>
2017-06-25 22:09:23 -0500

    initial commit

---
+++ declare.js
@@ -0,0 +1,29 @@
+var num1=7;
+var num2=13;
+var num3=6.021023;
+var num4=1313;
+
+var string1="Hot ham & Cheese";
+var string2="Chopper";
+var string3="#NerdAF";
+
+var isBaldMikeBald=true;
+var doesABearNotShitInTheWoods=false;
+
+var lostInSpace;
+
+console.log("Your favorite number is ", num1);
+console.log("My favorite number is ", num2);
+console.log("Avogadro's number is ", num3);
+console.log("The Munster's address is " num4);
+
+console.log("I love me a ", string1);
+console.log("I love ", string2);
+console.log("I am rapidly becoming ", string3);
+
+console.log("you love me =", isBaldMikeBald);
+console.log("You smell nice = ", doesABearNotShitInTheWoods);
+
+console.log("unknown", lostInSpace);
+
+
